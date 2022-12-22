92af399770499d7af23fa0c7b921d687ac5bd92e
Steven perry <Sperry3006@gmail.com>
2022-12-22 13:54:04 +0000

    fixed individual recipe routing

--- src/components/recipes/RecipesList.js
+++ src/components/recipes/RecipesList.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Recipe from "./Recipe";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const RecipeLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
 
 const RecipesList = ({ recipes }) => {
   const recipeComponents = recipes.map((recipe) => {
     return (
-      <>
-        <Link to={`/recipe/${recipe.id}`}>
-          <Recipe key={recipe.id} recipe={recipe} />
-        </Link>
-      </>
+      <RecipeLink to={`/recipe/${recipe.id}`}>
+        <Recipe key={recipe.id} recipe={recipe} />
+      </RecipeLink>
     );
   });
 
--- src/dummyData/RecipesData.js
+++ src/dummyData/RecipesData.js
@@ -2,6 +2,8 @@ const recipesData = [
   {
     id: 1,
     name: "Spaghetti Bolognese",
+    description:
+      "Everyone needs a basic spaghetti bolognese recipe that still tastes great, no matter how simple. Get that depth of flavour by cooking the sauce very gently until it’s super rich. This spag bol is designed to be a low cost recipe.",
     calories: 297,
     rating: 4,
     image: "spaghetti_bolognese.jpeg",
@@ -9,6 +11,8 @@ const recipesData = [
   {
     id: 2,
     name: "Lentil Soup",
+    description:
+      "This simple soup can be made overnight for a tasty lunch the next day. It's hearty and filling, packed with nutritious ingredients. Want to keep it vegan? Just forego the cheese topping.",
     calories: 56,
     rating: 3,
     image: "slow_cooker_lentil_soup.jpeg",
@@ -16,6 +20,8 @@ const recipesData = [
   {
     id: 3,
     name: "Slow Cooker Dhal",
+    description:
+      "Tasty and filling, dal makes a cheap, healthy and satisfying meal.",
     calories: 356,
     rating: 4,
     image: "slow_cooker_split_pea.jpeg",
@@ -23,6 +29,8 @@ const recipesData = [
   {
     id: 4,
     name: "Vegetable Pakoras",
+    description:
+      "Classic Indian finger food that no one can resist, these easy vegetable pakoras are so simple to make.",
     calories: 200,
     rating: 4,
     image: "vegetablepakoras.jpeg",
@@ -30,6 +38,8 @@ const recipesData = [
   {
     id: 5,
     name: "Low Fat Chicken Tikka Massala",
+    description:
+      "Making your own tikka masala sauce isn’t so tricky, so enjoy a lower calorie version of this favourite dish.",
     calories: 514,
     rating: 5,
     image: "low-fat_chicken_tikka.jpeg",
@@ -37,6 +47,8 @@ const recipesData = [
   {
     id: 6,
     name: "Patatas Bravas",
+    description:
+      "Patatas bravas as they should be: crisp, olive oil-fried potatoes topped with a tomato sauce, well-flavoured with hot pimentón and sharpened with a little vinegar.",
     calories: 405,
     rating: 3,
     image: "patatas_bravas.jpeg",
@@ -44,6 +56,8 @@ const recipesData = [
   {
     id: 7,
     name: "Easy Spanish Chicken",
+    description:
+      "Healthy chicken breasts and butter beans get a flavour boost from a little bit of cooking chorizo and red wine. Green beans are excellent when in season, but you can also use broccoli, green cabbage or kale on the side.",
     calories: 352,
     rating: 4,
     image: "easy_spanish_chicken.jpeg",
