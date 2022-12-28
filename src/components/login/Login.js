bc2e5c9a2b4d5a8628639a3c8a5f71b761b90c90
sreadytocode <sara.younis2303@gmail.com>
2022-12-28 13:56:49 +0000

    Started on sidebar

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,20 +2,18 @@ import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from "../../containers/Footer";
 import "./Login.css";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { LoginNavbar, Sidebar, UserProfile } from "./loginComponents";
 import { Calendar, ShoppingList } from "./loginpages";
 import Title from "../reusable/Title";
 
 const Login = () => {
-  const activeMenu = false;
+  const activeMenu = true;
   return (
     <div>
-      <div className="login">
-        <Title text = {"login"} />
-      </div>
       {activeMenu ? (
-        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+        <div className="w-500 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
 
           <Sidebar/>
       </div>
@@ -25,9 +23,9 @@ const Login = () => {
         </div>
       )}
       <div className={activeMenu ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  ' : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '}>
-          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+          {/* <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
             <LoginNavbar/>
-          </div>
+          </div> */}
       </div>
       <div>
         <Routes>
--- src/components/login/loginComponents/Sidebar.jsx
+++ src/components/login/loginComponents/Sidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  
+  const activeMenu = true;
 
-export default Sidebar
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link to="/" onClick={()=>{}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+              <span>Dashboard</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
