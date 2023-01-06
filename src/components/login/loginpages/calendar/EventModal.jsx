444556c213226e052169c42ddb13b16f463dc117
sreadytocode <sara.younis2303@gmail.com>
2023-01-06 19:32:49 +0000

    Fixed css for eventmodal

--- src/components/login/loginpages/calendar/EventModal.jsx
+++ src/components/login/loginpages/calendar/EventModal.jsx
@@ -23,35 +23,38 @@ const EventModal = () => {
       };
 
   return (
-    <div className="w-full fixed left-0 top-0 flex justify-center items-center">
+    <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
-        <header className="bg-gray-100 px-4 py-2 flex justify-end">
+        <header className="px-4 py-2 flex justify-end">
           <div className="text-gray-400">
             {selectedEvent && (
               <button
+              className='w-10 bg-red-500 mr-1'
                 onClick={() => {
                   dispatchCalEvent({ type: "delete", payload: selectedEvent });
                   setShowEventModal(false);
                 }}
               >
-                <MdDeleteForever />
+                <MdDeleteForever className='w-9'/>
               </button>
             )}
-            <button onClick={() => setShowEventModal(false)}>
-              <MdClose />
+            <button onClick={() => setShowEventModal(false)} className="w-10 bg-green-500">
+              <MdClose className='w-9'/>
             </button>
           </div>
         </header>
         <div className="p-3">
           <div className="grid grid-cols-1/5 items-end gap-y-7">
-            <div> </div>
+            <div> 
+              <h1 className='font-semibold text-xl'>Enter your recipe</h1>
+            </div>
             <input
               type="text"
               name="title"
               placeholder="Add Recipe"
               value={title}
               required
-              className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 border-b-2 border-gray-200 focus:outline-none focus:ring-0 "
               onChange={(e) => setTitle(e.target.value)}
             />
             <p>{daySelected.format("dddd, MMMM DD")}</p>
@@ -61,7 +64,7 @@ const EventModal = () => {
           <button
             type="submit"
             onClick={handleSubmit}
-            className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white"
+            className="bg-green-500 hover:bg-green-800 px-6 py-2 rounded text-white flex"
           >
             Save
           </button>
