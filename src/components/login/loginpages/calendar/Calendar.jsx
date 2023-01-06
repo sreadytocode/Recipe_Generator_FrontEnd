06c3c82ba03e8c812b259a6a7880bffb1b3a678a
sreadytocode <sara.younis2303@gmail.com>
2023-01-06 15:56:36 +0000

    Got calendarHeader to work and integrated in calendar component

--- src/components/login/loginpages/calendar/Calendar.jsx
+++ src/components/login/loginpages/calendar/Calendar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import Login from "../Login";
-import "../Login.css";
+import Login from "../../Login";
+import "../../Login.css";
+import { useStateContext } from '../../contexts/ContextProvider';
+import CalendarHeader from './CalendarHeader';
 
 const Calendar = () => {
+  const { monthIndex, showEventModal } = useStateContext();
+
   return (
     <div className="grid grid-cols-5 gap-1">
       <div className="col-start-1 col-end-2">
@@ -10,6 +14,7 @@ const Calendar = () => {
       </div>
       <div className="col-start-2 col-end-6">
         <h1>Calendar</h1>
+        <CalendarHeader/>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit explicabo, saepe ullam assumenda, doloremque rerum accusamus sapiente eaque quas autem natus adipisci deleniti odit eos suscipit error, ipsam officia fugit?</p>
       </div> 
     </div>
