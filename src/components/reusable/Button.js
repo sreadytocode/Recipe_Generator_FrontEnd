2a334268e0eede62b1dfeea4645525a8f5894004
Illyriat <109182598+Illyriat@users.noreply.github.com>
2023-01-09 17:08:49 +0000

    login using auth0

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Button() {
+  const { loginWithRedirect } = useAuth0();
+  
+
+
   return (
     <Link to="login">
-      <button className="btn login-btn">
+      <button className="btn login-btn" onClick={() => loginWithRedirect()}>
         <i className="fas fa-user mr-2" />
         Login
       </button>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from '../src/components/login/contexts/ContextProvider'
+import { Auth0Provider } from "@auth0/auth0-react";
+import { getConfig } from "./config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const onRedirectCallback = (appState) => {
+    history.push(
+      appState && appState.returnTo ? appState.returnTo : window.location.pathname
+    );
+  };
+  
+  // Please see https://auth0.github.io/auth0-react/interfaces/Auth0ProviderOptions.html
+  // for a full list of the available properties on the provider
+  const config = getConfig();
+  
+  const providerConfig = {
+    domain: config.domain,
+    clientId: config.clientId,
+    ...(config.audience ? { audience: config.audience } : null),
+    redirectUri: window.location.origin,
+    onRedirectCallback,
+  };
+
+
 root.render(
     <React.StrictMode>
-            <ContextProvider>
-                 <App />   
-            </ContextProvider>
+            <Auth0Provider {...providerConfig}>
+                <ContextProvider>
+                    <App />   
+                </ContextProvider>
+            </Auth0Provider>
     </React.StrictMode>
         );
 
